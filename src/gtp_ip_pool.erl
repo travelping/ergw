c95a614b8adebcbaeda455414b143b7d4e3a1eea
Andreas Schultz <aschultz@tpip.net>
2016-10-18 14:02:00 +0200

    replace IP allocator with constant time variant
    Searching for a free IP takes in the worst case pool size - 1
    operations. This leads to an unpredictable timing behavior of
    the pool allocator.
    Replace that with an constant time allocator.

--- src/gtp_ip_pool.erl
+++ src/gtp_ip_pool.erl
@@ -17,7 +17,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
 
--record(state, {type, base, shift, size, used, free,  tid}).
+-record(state, {type, base, shift, size, free, max_alloc, used_pool, free_pool}).
 -record(lease, {ip, client_id}).
 
 -include_lib("stdlib/include/ms_transform.hrl").
@@ -43,7 +43,8 @@ release(Server, IP) ->
 %%%===================================================================
 
 init({Prefix, PrefixLen}) ->
-    Tid = ets:new(pool, [ordered_set, {keypos, #lease.ip}]),
+    UsedTid = ets:new(used_pool, [set, {keypos, #lease.ip}]),
+    FreeTid = ets:new(free_pool, [set, {keypos, #lease.ip}]),
     {Type, PrefixLen1, Shift} = case size(Prefix) of
 				    4 -> {ipv4,  32 - PrefixLen,  0};
 				    8 -> {ipv6, 128 - PrefixLen, 64}
@@ -53,41 +54,46 @@ init({Prefix, PrefixLen}) ->
 		   base  = ip2int(Prefix),
 		   shift = Shift,
 		   size  = Size,
-		   used  = 0,
-		   free  = Size,
-		   tid   = Tid},
+		   free  = 0,
+		   max_alloc = 0,
+		   used_pool = UsedTid,
+		   free_pool = FreeTid},
     lager:debug("init Pool state: ~p", [lager:pr(State, ?MODULE)]),
     {ok, State}.
 
-handle_call({allocate, _ClientId}, _From,
-	    #state{free = 0} = State) ->
-    {reply, {error, full}, State};
+handle_call({allocate, ClientId} = Request, _From,
+	    #state{size = Size, free = Free, max_alloc = MaxAlloc,
+		   used_pool = UsedTid} = State0)
+  when MaxAlloc < Size andalso Free == 0 ->
+    lager:debug("~w: Allocate: ~p, State: ~p", [self(), lager:pr(Request, ?MODULE), lager:pr(State0, ?MODULE)]),
+
+    ets:insert(UsedTid, #lease{ip = MaxAlloc, client_id = ClientId}),
+    State = State0#state{max_alloc = MaxAlloc + 1},
+    IP = id2ip(MaxAlloc, State),
+
+    {reply, {ok, IP}, State};
 
 handle_call({allocate, ClientId} = Request, _From,
-	    #state{type = Type, base  = Base, shift = Shift,
-		   size = Size, used  = Used, free  = Free,
-		   tid  = Tid} = State) ->
-    lager:warning("~w: Allocate: ~p, State: ~p", [self(), lager:pr(Request, ?MODULE), lager:pr(State, ?MODULE)]),
+	    #state{free = Free, free_pool = FreeTid, used_pool = UsedTid} = State)
+  when Free =/= 0 ->
+    lager:debug("~w: Allocate: ~p, State: ~p", [self(), lager:pr(Request, ?MODULE), lager:pr(State, ?MODULE)]),
 
-    Init = hash(ClientId, Size),
-    case try_alloc(Init, Size, Tid) of
-	Final when is_integer(Final) ->
-	    ets:insert(Tid, #lease{ip = Final, client_id = ClientId}),
-	    lager:debug("Final: ~p", [Final]),
-	    IP = int2ip(Type, Base + (Final bsl Shift)),
-	    lager:debug("IP: ~p", [IP]),
+    Id = ets:first(FreeTid),
+    ets:delete(FreeTid, Id),
+    ets:insert(UsedTid, #lease{ip = Id, client_id = ClientId}),
+    IP = id2ip(Id, State),
 
-	    {reply, {ok, IP}, State#state{used = Used + 1, free = Free - 1}};
-	Other ->
-	    {reply, Other, State}
-    end;
+    {reply, {ok, IP}, State};
+
+handle_call({allocate, _ClientId}, _From, State) ->
+    {reply, {error, full}, State};
 
 handle_call({release, {_,_,_,_} = IPv4}, _From, State0) ->
-    State1 = do_release(ip2int(IPv4), State0),
+    State1 = release_ip(ip2int(IPv4), State0),
     {reply, ok, State1};
 
 handle_call({release, {{_,_,_,_,_,_,_,_} = IPv6, _Len}}, _From, State0) ->
-    State1 = do_release(ip2int(IPv6), State0),
+    State1 = release_ip(ip2int(IPv6), State0),
     {reply, ok, State1};
 
 handle_call(Request, _From, State) ->
@@ -112,18 +118,6 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 
-int2list(I) ->
-    int2list(I, []).
-
-int2list(0, L) ->
-    lists:reverse(L);
-int2list(I, L) ->
-    int2list(I div 256, [I rem 256|L]).
-
-hash(Id, Size) ->
-    <<Hash:160>> = crypto:hash(sha, int2list(Id)),
-    Hash rem Size.
-
 ip2int({A, B, C, D}) ->
     (A bsl 24) + (B bsl 16) + (C bsl 8) + D;
 ip2int({A, B, C, D, E, F, G, H}) ->
@@ -137,41 +131,23 @@ int2ip(ipv6, IP) ->
     <<A:16, B:16, C:16, D:16, E:16, F:16, G:16, H:16>> = <<IP:128>>,
     {{A, B, C, D, E, F, G, H}, 64}.
 
-try_alloc(Init, Size, Tid) ->
-    MS = ets:fun2ms(fun(#lease{ip = IP} = Lease) when IP >= Init -> Lease end),
-    Next = ets:select(Tid, MS, 1),
-    lager:debug("try_alloc: got ~p", [Next]),
-    try_alloc(Init, Size, Next, Tid).
+id2ip(Id, #state{type = Type, base = Base, shift = Shift}) ->
+    int2ip(Type, Base + (Id bsl Shift)).
 
-try_alloc(Value, Size, '$end_of_table', _)
-  when Value < Size ->
-    Value;
-try_alloc(Value, Size, '$end_of_table', Tid)
-  when Value >= Size ->
-    try_alloc(0, Size, Tid);
-try_alloc(Value, _Size, {[#lease{ip = IP}], _Continuation}, _Tid)
-  when IP > Value ->
-    Value;
-try_alloc(Value, Size, {[#lease{ip = Value}], Continuation}, Tid) ->
-    Next = ets:select(Continuation),
-    lager:debug("try_alloc: got ~p", [Next]),
-    try_alloc(Value + 1, Size, Next, Tid).
-
-
-do_release(IP, #state{type = Type, base = Base, shift = Shift,
-		      size = Size, used = Used, free  = Free,
-		      tid  = Tid} = State)
+release_ip(IP, #state{base = Base, shift = Shift, size = Size,
+		      used_pool = UsedTid, free_pool = FreeTid} = State)
   when IP >= Base andalso IP =< Base + (Size bsl Shift) ->
-    Offs = (IP - Base) bsr Shift,
-    case ets:lookup(Tid, Offs) of
+    Id = (IP - Base) bsr Shift,
+
+    case ets:lookup(UsedTid, Id) of
 	[_] ->
-	    ets:delete(Tid, Offs),
-	    State#state{free = Free + 1, used = Used - 1};
+	    ets:delete(UsedTid, IP),
+	    ets:insert(FreeTid, #lease{ip = Id});
 	_ ->
-	    lager:warning("release of unallocated IP: ~p, Offs: ~p, ETS: ~p", [int2ip(Type, IP), Offs, ets:tab2list(Tid)]),
-	    State
-    end;
-do_release(IP, #state{type = Type, base = Base, shift = Shift, size = Size} = State) ->
+	    lager:warning("release of unallocated IP: ~p", [id2ip(Id, State)])
+    end,
+    State;
+release_ip(IP, #state{type = Type, base = Base, shift = Shift, size = Size} = State) ->
     lager:warning("release of out-of-pool IP: ~p, ~w < ~w < ~w",
 		  [int2ip(Type, IP), Base, IP, Base + (Size bsl Shift)]),
     State.
