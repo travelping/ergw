bfb08e2eb619984905dd35e3e28f9d51fb2ecd57
Andreas Schultz <aschultz@tpip.net>
2017-03-28 09:39:55 +0200

    correct LICENSE header on test suites

--- test/pgw_SUITE.erl
+++ test/pgw_SUITE.erl
@@ -1,8 +1,9 @@
-%% This Source Code Form is subject to the terms of the Mozilla Public
-%% License, v. 2.0. If a copy of the MPL was not distributed with this
-%% file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 %% Copyright 2017, Travelping GmbH <info@travelping.com>
+
+%% This program is free software; you can redistribute it and/or
+%% modify it under the terms of the GNU General Public License
+%% as published by the Free Software Foundation; either version
+%% 2 of the License, or (at your option) any later version.
 
 -module(pgw_SUITE).
 
