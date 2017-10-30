7a5c79439086dc3142f7c1b57f9acd00a1ca7811
Andreas Schultz <aschultz@tpip.net>
2017-10-30 13:40:29 +0100

    add helper script to run rebar3 shell with heightened capabilities

---
+++ priv/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# capsh from libcap2 git master (newer that 2.25) is needed
+#
+
+cmd="rebar3 shell --setcookie secret --sname gtp-c-node --apps ergw $@"
+
+sudo capsh --caps="cap_net_raw,cap_sys_admin+eip cap_setpcap,cap_setuid,cap_setgid+ep" --keep=1 --user=$USER --addamb=cap_net_raw,cap_sys_admin -- -c "$cmd"
