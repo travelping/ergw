e489fc60aef546dd2874df1011f29792e874b285
Andreas Schultz <andreas.schultz@travelping.com>
2018-01-04 14:57:29 +0100

    implement binary Sx API

--- src/ergw_sup.erl
+++ src/ergw_sup.erl
@@ -10,7 +10,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, start_sx_socket/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -24,6 +24,9 @@
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+start_sx_socket(Opts) ->
+    {ok, _} = supervisor:start_child(?MODULE, ?CHILD(ergw_sx_socket, worker, [Opts])).
 
 %% ===================================================================
 %% Supervisor callbacks
