091de829e3a57af20f520bc19e67e5788dc8eb7a
Andreas Schultz <aschultz@tpip.net>
2018-02-07 15:47:02 +0000

    update Sx binary API to used Sx Assoc Setup Request
    * switch Sx binary API module to gen_statem
    * use Sx Association Setup Request to get UE IP resources
      from DP node

--- src/ergw_sx_bin.erl
+++ src/ergw_sx_bin.erl
@@ -7,16 +7,16 @@
 
 -module(ergw_sx_bin).
 
--behavior(gen_server).
+-behavior(gen_statem).
 -behavior(ergw_sx_api).
 
 %% API
 -export([validate_options/1,
 	 start_link/1, send/4, get_id/1,
-	 call/2, heartbeat_response/2]).
+	 call/2, response/3]).
 
 %% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+-export([init/1, callback_mode/0, handle_event/4,
 	 terminate/2, code_change/3]).
 
 -include_lib("kernel/include/inet.hrl").
@@ -24,14 +24,14 @@
 -include_lib("pfcplib/include/pfcp_packet.hrl").
 -include("include/ergw.hrl").
 
--record(state, {state, tref, timeout, name, node, ip, pid, gtp_port}).
+-record(data, {timeout, name, cp, dp, gtp_port}).
 
 %%====================================================================
 %% API
 %%====================================================================
 
 start_link({Name, SocketOpts}) ->
-    gen_server:start_link(?MODULE, [Name, SocketOpts], []).
+    gen_statem:start_link(?MODULE, [Name, SocketOpts], []).
 
 send(GtpPort, IP, Port, Data) ->
     cast(GtpPort, {send, IP, Port, Data}).
@@ -42,8 +42,8 @@ get_id(GtpPort) ->
 call(Context, Request) ->
     dp_call(Context, Request).
 
-heartbeat_response(Pid, Response) ->
-    gen_server:cast(Pid, {heartbeat, Response}).
+response(Pid, Type, Response) ->
+    gen_statem:cast(Pid, {Type, Response}).
 
 %%%===================================================================
 %%% Options Validation
@@ -83,14 +83,14 @@ validate_option(Opt, Value) ->
 %% TODO: GTP data path handler is currently not working!!
 cast(#gtp_port{pid = Handler}, Request)
   when is_pid(Handler) ->
-    gen_server:cast(Handler, Request);
+    gen_statem:cast(Handler, Request);
 cast(GtpPort, Request) ->
     lager:warning("GTP DP Port ~p, CAST Request ~p not implemented yet",
 		  [lager:pr(GtpPort, ?MODULE), lager:pr(Request, ?MODULE)]).
 
 call_port(#gtp_port{pid = Handler}, Request)
   when is_pid(Handler) ->
-    gen_server:call(Handler, Request);
+    gen_statem:call(Handler, Request);
 call_port(GtpPort, Request) ->
     lager:warning("GTP DP Port ~p, CAST Request ~p not implemented yet",
 		  [lager:pr(GtpPort, ?MODULE), lager:pr(Request, ?MODULE)]).
@@ -104,127 +104,123 @@ dp_call(#context{data_port = GtpPort}, Request) ->
 %%% gen_server callbacks
 %%%===================================================================
 
-init([Name, #{node := Node, ip := IP}]) ->
-    State = #state{state = disconnected,
-		   tref = undefined,
-		   timeout = 10,
-		   name = Name,
-		   node = Node,
-		   ip = IP},
-    send_heartbeat(State),
-    {ok, State}.
+%% callback_mode() -> [handle_event_function, state_enter].
+callback_mode() -> handle_event_function.
 
-handle_call(#pfcp{} = Request, _From, #state{node = Node} = State) ->
+init([Name, #{node := Node, ip := IP}]) ->
+    {ok, CP} =ergw_sx_socket:id(),
+    Data = #data{timeout = 10,
+		 name = Name,
+		 cp = CP,
+		 dp = #node{node = Node, ip = IP}},
+    {ok, disconnected, Data, [{next_event, internal, setup}]}.
+
+handle_event(_, setup, disconnected, #data{cp = CP, dp = #node{node = Node}}) ->
+    IEs = [node_id(CP),
+	   #recovery_time_stamp{
+	      time = seconds_to_sntp_time(gtp_config:get_start_time())}],
+    Req = #pfcp{version = v1, type = association_setup_request, ie = IEs},
+    ergw_sx_socket:call(Node, 500, 5, Req, response_cb(association_setup_request)),
+    keep_state_and_data;
+
+handle_event(cast, {association_setup_request, timeout}, disconnected, _Data) ->
+    {keep_state_and_data, [{state_timeout, 5000, setup}]};
+
+handle_event(cast, {_, #pfcp{version = v1, type = association_setup_response, ie = IEs}},
+	     disconnected, Data0) ->
+    case IEs of
+	#{pfcp_cause := #pfcp_cause{cause = 'Request accepted'}} ->
+	    Data = handle_nodeup(IEs, Data0),
+	    {next_state, connected, Data, [next_heartbeat(Data)]};
+	Other ->
+	    lager:warning("Other: ~p", [lager:pr(Other, ?MODULE)]),
+	    {keep_state_and_data, [{state_timeout, 5000, setup}]}
+    end;
+
+%%
+%% heartbeat logic
+%%
+handle_event(state_timeout, heartbeat, connected, Data) ->
+    lager:warning("sending heartbeat"),
+    send_heartbeat(Data),
+    keep_state_and_data;
+
+handle_event(cast, {heartbeat, timeout}, connected, Data0) ->
+    Data = handle_nodedown(Data0),
+    {next_state, disconnected, Data, [{next_event, internal, setup}]};
+
+handle_event(cast, {_, #pfcp{version = v1, type = heartbeat_response}}, connected, Data) ->
+    {next_state, connected, Data, [next_heartbeat(Data)]};
+
+handle_event({call, From}, #pfcp{} = Request, connected, #data{dp = #node{node = Node}}) ->
     lager:debug("DP Call ~p", [lager:pr(Request, ?MODULE)]),
     Reply = ergw_sx_socket:call(Node, Request),
-    {reply, Reply, State};
+    {keep_state_and_data, [{reply, From, Reply}]};
 
-handle_call(get_id, _From, #state{pid = Pid} = State) ->
-    {reply, {ok, Pid}, State};
+handle_event({call, From}, #pfcp{}, _, _Data) ->
+    Reply = {error, not_connected},
+    {keep_state_and_data, [{reply, From, Reply}]};
 
-handle_call(Request, _From, State) ->
-    lager:error("handle_call: unknown ~p", [lager:pr(Request, ?MODULE)]),
-    {reply, ok, State}.
+handle_event({call, From}, get_id, _, _Data) ->
+    Reply =  {ok, self()},
+    {keep_state_and_data, [{reply, From, Reply}]}.
 
-handle_cast({send, _IP, _Port, _Data} = Msg, #state{pid = Pid} = State) ->
-    lager:debug("DP Cast ~p: ~p", [Pid, Msg]),
-    gen_server:cast(Pid, Msg),
-    {noreply, State};
+%% handle_event(cast, {send, _IP, _Port, _Data} = Msg, _, #data{pid = Pid}) ->
+%%     lager:debug("DP Cast ~p: ~p", [Pid, Msg]),
+%%     gen_server:cast(Pid, Msg),
+%%     keep_state_and_data.
 
-handle_cast({heartbeat, timeout}, State0) ->
-    lager:warning("peer node down"),
-
-    State1 = handle_nodedown(State0),
-    State = shedule_next_heartbeat(State1#state{tref = undefined}),
-    {noreply, State};
-
-handle_cast({heartbeat, #pfcp{version = v1, type = heartbeat_response}},
-	    #state{state = disconnected} = State0) ->
-
-    State1 = handle_nodeup(State0),
-    State = shedule_next_heartbeat(State1),
-    {noreply, State};
-
-handle_cast({heartbeat, #pfcp{version = v1, type = heartbeat_response}}, State0) ->
-    State = shedule_next_heartbeat(State0),
-    {noreply, State};
-
-handle_cast(Msg, State) ->
-    lager:error("handle_cast: unknown ~p", [lager:pr(Msg, ?MODULE)]),
-    {noreply, State}.
-
-handle_info(heartbeat, State) ->
-    lager:warning("sending heartbeat"),
-    send_heartbeat(State),
-    {noreply, State#state{tref = undefined}};
-
-handle_info(Info, State) ->
-    lager:error("handle_info: unknown ~p, ~p",
-		[lager:pr(Info, ?MODULE), lager:pr(State, ?MODULE)]),
-    {noreply, State}.
-
-terminate(_Reason, _State) ->
+terminate(_Reason, _Data) ->
     ok.
 
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+code_change(_OldVsn, Data, _Extra) ->
+    {ok, Data}.
 
 %%%===================================================================
 %%% Sx Msg handler functions
 %%%===================================================================
 
 %% handle_msg(#pfcp{type = heartbeat_response},
-%% 	   #state{state = disconnected} = State0) ->
-%%     State1 = cancel_timeout(State0),
-%%     State2 = handle_nodeup(State1),
-%%     State = shedule_next_heartbeat(State2),
-%%     {noreply, State};
+%% 	   #data{state = disconnected} = Data0) ->
+%%     Data1 = cancel_timeout(Data0),
+%%     Data2 = handle_nodeup(Data1),
+%%     Data = shedule_next_heartbeat(Data2),
+%%     {noreply, Data};
 
-%% handle_msg(#pfcp{type = heartbeat_response}, State0) ->
-%%     State1 = cancel_timeout(State0),
-%%     State = shedule_next_heartbeat(State1),
-%%     {noreply, State};
+%% handle_msg(#pfcp{type = heartbeat_response}, Data0) ->
+%%     Data1 = cancel_timeout(Data0),
+%%     Data = shedule_next_heartbeat(Data1),
+%%     {noreply, Data};
 
 %% handle_msg(#pfcp{type = session_report_request} = Report,
-%% 	   #state{gtp_port = GtpPort} = State) ->
+%% 	   #data{gtp_port = GtpPort} = Data) ->
 %%     lager:debug("handle_info: ~p, ~p",
-%% 		[lager:pr(Report, ?MODULE), lager:pr(State, ?MODULE)]),
+%% 		[lager:pr(Report, ?MODULE), lager:pr(Data, ?MODULE)]),
 %%     gtp_context:session_report(GtpPort, Report),
-%%     {noreply, State};
+%%     {noreply, Data};
 
-%% handle_msg(Msg, #state{pending = From} = State0)
+%% handle_msg(Msg, #data{pending = From} = Data0)
 %%   when From /= undefined ->
-%%     State = cancel_timeout(State0),
+%%     Data = cancel_timeout(Data0),
 %%     gen_server:reply(From, Msg),
-%%     {noreply, State#state{pending = undefined}};
+%%     {noreply, Data#data{pending = undefined}};
 
-%% handle_msg(Msg, State) ->
-%%     lager:error("handle_msg: unknown ~p, ~p", [Msg, lager:pr(State, ?MODULE)]),
-%%     {noreply, State}.
+%% handle_msg(Msg, Data) ->
+%%     lager:error("handle_msg: unknown ~p, ~p", [Msg, lager:pr(Data, ?MODULE)]),
+%%     {noreply, Data}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-
-%% cancel_timer(Ref) ->
-%%     case erlang:cancel_timer(Ref) of
-%% 	false ->
-%% 	    receive {timeout, Ref, _} -> 0
-%% 	    after 0 -> false
-%% 	    end;
-%% 	RemainingTime ->
-%% 	    RemainingTime
-%%     end.
-
-%% cancel_timeout(#state{tref = TRef} = State) ->
-%%     cancel_timer(TRef),
-%%     State#state{tref = undefined}.
 
 %% sntp_time_to_seconds(Time) ->
 %%      case Time band 16#80000000 of
 %% 	 0 -> Time + 2085978496; % use base: 7-Feb-2036 @ 06:28:16 UTC
 %% 	 _ -> Time - 2208988800  % use base: 1-Jan-1900 @ 01:00:00 UTC
 %%      end.
+
+response_cb(Type) ->
+    {?MODULE, response, [self(), Type]}.
 
 seconds_to_sntp_time(Sec) ->
     if Sec >= 2085978496 ->
@@ -233,50 +229,39 @@ seconds_to_sntp_time(Sec) ->
 	    Sec + 2208988800
     end.
 
-shedule_next_heartbeat(State = #state{state = ConnState, timeout = Timeout}) ->
-    NewTimeout =
-	case ConnState of
-	    disconnected
-	      when Timeout < 3000 ->
-		Timeout * 2;
-	    disconnected ->
-		Timeout;
-	    _ ->
-		5000
-	end,
-    TRef = erlang:send_after(Timeout, self(), heartbeat),
-    State#state{tref = TRef, timeout = NewTimeout}.
+next_heartbeat(_Data) ->
+    {state_timeout, 5000, heartbeat}.
 
-send_heartbeat(#state{node = Node}) ->
+node_id(#node{node = Node})
+  when is_atom(Node) ->
+    #node_id{id = string:split(atom_to_binary(Node, utf8), ".", all)}.
+
+send_heartbeat(#data{dp = #node{node = Node}}) ->
     IEs = [#recovery_time_stamp{
 	      time = seconds_to_sntp_time(gtp_config:get_start_time())}],
     Req = #pfcp{version = v1, type = heartbeat_request, ie = IEs},
-    ergw_sx_socket:call(Node, 500, 5, Req, {?MODULE, heartbeat_response, [self()]}).
+    ergw_sx_socket:call(Node, 500, 5, Req, response_cb(heartbeat)).
 
-handle_nodeup(#state{name = Name, node = Node, ip = IP} = State) ->
+handle_nodeup(#{user_plane_ip_resource_information :=
+		    #user_plane_ip_resource_information{
+		       ipv4 = IPv4, ipv6 = IPv6
+		      }
+	       } = _IEs, #data{name = Name, dp = #node{node = Node} = DP} = Data) ->
     lager:warning("Node ~s is up", [inet:ntoa(Node)]),
+    lager:warning("Node IEs: ~p", [lager:pr(_IEs, ?MODULE)]),
 
-    %% {ok, Pid, IP} = bind(Node, RemoteName),
-    %% ok = clear(Pid),
-    Pid = self(),
+    IP =
+	if IPv4 /= undefined -> gtp_c_lib:bin2ip(IPv4);
+	   IPv6 /= undefined -> gtp_c_lib:bin2ip(IPv6)
+	end,
 
     {ok, RCnt} = gtp_config:get_restart_counter(),
     GtpPort = #gtp_port{name = Name, type = 'gtp-u', pid = self(),
 			ip = IP, restart_counter = RCnt},
     gtp_socket_reg:register(Name, GtpPort),
 
-    State#state{state = connected, timeout = 100, ip = IP, pid = Pid, gtp_port = GtpPort}.
+    Data#data{timeout = 100, gtp_port = GtpPort, dp = DP#node{ip = IP}}.
 
-handle_nodedown(#state{name = Name} = State) ->
+handle_nodedown(#data{name = Name} = Data) ->
     gtp_socket_reg:unregister(Name),
-    State#state{state = disconnected}.
-
-%%%===================================================================
-%%% Data Path Remote API
-%%%===================================================================
-
-clear(Pid) ->
-    gen_server:call(Pid, clear).
-
-bind(Node, Port) ->
-    gen_server:call({'gtp-u', Node}, {bind, Port}).
+    Data.
