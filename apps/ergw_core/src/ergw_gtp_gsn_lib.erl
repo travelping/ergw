3df6fa8b6d5d908102c4386b8ad5a3c9117cfdec
Andreas Schultz <andreas.schultz@travelping.com>
2021-07-05 15:11:14 +0200

    convert SAE-GW Create Session Request to non-blocking FSM

--- apps/ergw_core/src/ergw_gtp_gsn_lib.erl
+++ apps/ergw_core/src/ergw_gtp_gsn_lib.erl
@@ -11,7 +11,7 @@
 	  {parse_transform, do},
 	  {parse_transform, cut}]).
 
--export([connect_upf_candidates/4, create_session/10, create_session_m/5]).
+-export([connect_upf_candidates/4, create_session_m/5]).
 -export([triggered_charging_event/4, usage_report/3, close_context/3, close_context/4]).
 -export([handle_peer_change/3, update_tunnel_endpoint/2,
 	 apply_bearer_change/5]).
@@ -34,128 +34,6 @@ connect_upf_candidates(APN, Services, NodeSelect, PeerUpNode) ->
     SxConnectId = ergw_sx_node:request_connect(Candidates, NodeSelect, 1000),
 
     {ok, {Candidates, SxConnectId}}.
-
-create_session(APN, PAA, DAF, UPSelInfo, Session, SessionOpts, Context, LeftTunnel, LeftBearer, PCC) ->
-    try
-	{ok, create_session_fun(APN, PAA, DAF, UPSelInfo, Session, SessionOpts, Context, LeftTunnel, LeftBearer, PCC)}
-    catch
-	throw:Error ->
-	    {error, Error}
-    end.
-
-create_session_fun(APN, PAA, DAF, {Candidates, SxConnectId}, Session,
-		   SessionOpts0, Context0, LeftTunnel, LeftBearer, PCC0) ->
-
-    ergw_sx_node:wait_connect(SxConnectId),
-
-    APNOpts =
-	case ergw_apn:get(APN) of
-	    {ok, Result2} -> Result2;
-	    {error, Err2} -> throw(Err2#ctx_err{context = Context0, tunnel = LeftTunnel})
-	end,
-
-    {UPinfo, SessionOpts1} =
-	case ergw_pfcp_context:select_upf(Candidates, SessionOpts0, APNOpts) of
-	    {ok, Result3} -> Result3;
-	    {error, Err3} -> throw(Err3#ctx_err{context = Context0, tunnel = LeftTunnel})
-	end,
-
-    {SessionOpts2, AuthSEvs} =
-	case ergw_gtp_gsn_session:authenticate(Session, SessionOpts1) of
-	    {ok, Result4} -> Result4;
-	    {error, Err4} -> throw(Err4#ctx_err{context = Context0, tunnel = LeftTunnel})
-	end,
-
-    {PCtx0, NodeCaps, RightBearer0} =
-	case ergw_pfcp_context:reselect_upf(Candidates, SessionOpts2, APNOpts, UPinfo) of
-	    {ok, Result5} -> Result5;
-	    {error, Err5} -> throw(Err5#ctx_err{context = Context0, tunnel = LeftTunnel})
-	end,
-
-    {Result6, {Cause, SessionOpts3, RightBearer, Context1}} =
-	ergw_gsn_lib:allocate_ips(PAA, APNOpts, SessionOpts2, DAF, LeftTunnel, RightBearer0, Context0),
-    case Result6 of
-	ok -> ok;
-	{error, Err6} -> throw(Err6#ctx_err{context = Context1, tunnel = LeftTunnel})
-    end,
-
-    Context = add_apn_timeout(APNOpts, SessionOpts3, Context1),
-
-    Bearer0 = #{left => LeftBearer, right => RightBearer},
-    Bearer1 =
-	case ergw_gsn_lib:assign_local_data_teid(left, PCtx0, NodeCaps, LeftTunnel, Bearer0) of
-	    {ok, Result7} -> Result7;
-	    {error, Err7} -> throw(Err7#ctx_err{context = Context, tunnel = LeftTunnel})
-	end,
-
-    ergw_aaa_session:set(Session, SessionOpts3),
-
-    Now = erlang:monotonic_time(),
-    SOpts = #{now => Now},
-
-    GxOpts = #{'Event-Trigger' => ?'DIAMETER_GX_EVENT-TRIGGER_UE_IP_ADDRESS_ALLOCATE',
-	       'Bearer-Operation' => ?'DIAMETER_GX_BEARER-OPERATION_ESTABLISHMENT'},
-
-    {_, GxEvents} =
-	case ergw_gtp_gsn_session:ccr_initial(Session, gx, GxOpts, SOpts) of
-	    {ok, Result8} -> Result8;
-	    {error, Err8} -> throw(Err8#ctx_err{context = Context, tunnel = LeftTunnel})
-	end,
-
-    RuleBase = ergw_charging:rulebase(),
-    {PCC1, PCCErrors1} = ergw_pcc_context:gx_events_to_pcc_ctx(GxEvents, '_', RuleBase, PCC0),
-    case ergw_pcc_context:pcc_ctx_has_rules(PCC1) of
-	true ->
-	    ok;
-	_ ->
-	    throw(?CTX_ERR(?FATAL, user_authentication_failed, tunnel = LeftTunnel))
-    end,
-
-    %% TBD............
-    CreditsAdd = ergw_pcc_context:pcc_ctx_to_credit_request(PCC1),
-    GyReqServices = #{credits => CreditsAdd},
-
-    {GySessionOpts, GyEvs} =
-	case ergw_gtp_gsn_session:ccr_initial(Session, gy, GyReqServices, SOpts) of
-	    {ok, Result9} -> Result9;
-	    {error, Err9} -> throw(Err9#ctx_err{context = Context, tunnel = LeftTunnel})
-	end,
-
-    ?LOG(debug, "GySessionOpts: ~p", [GySessionOpts]),
-    ?LOG(debug, "Initial GyEvs: ~p", [GyEvs]),
-
-    {_, _, RfSEvs} = ergw_aaa_session:invoke(Session, #{}, {rf, 'Initial'}, SOpts),
-
-    {PCC2, PCCErrors2} = ergw_pcc_context:gy_events_to_pcc_ctx(Now, GyEvs, PCC1),
-    PCC3 = ergw_pcc_context:session_events_to_pcc_ctx(AuthSEvs, PCC2),
-    PCC4 = ergw_pcc_context:session_events_to_pcc_ctx(RfSEvs, PCC3),
-
-    {PCtx, Bearer, SessionInfo} =
-	case ergw_pfcp_context:create_session(gtp_context, PCC4, PCtx0, Bearer1, Context) of
-	       {ok, Result10} -> Result10;
-	       {error, Err10} -> throw(Err10#ctx_err{context = Context, tunnel = LeftTunnel})
-	   end,
-
-    SessionOpts = maps:merge(SessionOpts3, SessionInfo),
-    ergw_aaa_session:invoke(Session, SessionOpts, start, SOpts#{async => true}),
-
-    GxReport = ergw_gsn_lib:pcc_events_to_charging_rule_report(PCCErrors1 ++ PCCErrors2),
-    if map_size(GxReport) /= 0 ->
-	    ergw_aaa_session:invoke(Session, GxReport,
-				    {gx, 'CCR-Update'}, SOpts#{async => true});
-       true ->
-	    ok
-    end,
-
-    case gtp_context:remote_context_register_new(LeftTunnel, Bearer, Context) of
-	ok ->
-	    {ok, Cause, SessionOpts, Context, Bearer, PCC4, PCtx};
-	{error, #ctx_err{level = Level, where = {File, Line}}} ->
-	    ?LOG(debug, #{type => ctx_err, level => Level, file => File,
-			  line => Line, reply => system_failure}),
-	    {error, system_failure, SessionOpts, Context, Bearer, PCC4, PCtx}
-    end.
-
 
 %% 'Idle-Timeout' received from ergw_aaa Session takes precedence over configured one
 add_apn_timeout(Opts, Session, Context) ->
@@ -350,9 +228,9 @@ add_apn_timeout(APNOpts) ->
     do([statem_m ||
 	   _ = ?LOG(debug, "~s", [?FUNCTION_NAME]),
 	   statem_m:lift(?LOG(debug, "AddApnTimeout: ~p~n", [APNOpts])),
-	   #{session_opts := SessionOpts0, context := Context0} <- statem_m:get_data(),
-	   {Context, SessionOpts} <- statem_m:return(add_apn_timeout(APNOpts, SessionOpts0, Context0)),
-	   statem_m:modify_data(_#{session_opts => SessionOpts, context => Context})
+	   #{session_opts := SessionOpts, context := Context0} <- statem_m:get_data(),
+	   Context <- statem_m:return(add_apn_timeout(APNOpts, SessionOpts, Context0)),
+	   statem_m:modify_data(_#{context => Context})
        ]).
 
 assign_local_data_teid(Key, PCtx, NodeCaps, RightBearer) ->
