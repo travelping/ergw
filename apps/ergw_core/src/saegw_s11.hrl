1521b40bdfa61701b8660a2e1510bf887aa08abd
Andreas Schultz <andreas.schultz@travelping.com>
2021-07-05 15:11:14 +0200

    convert SAE-GW Create Session Request to non-blocking FSM

---
+++ apps/ergw_core/src/saegw_s11.hrl
@@ -0,0 +1,25 @@
+-define('Cause',					{v2_cause, 0}).
+-define('Recovery',					{v2_recovery, 0}).
+-define('IMSI',						{v2_international_mobile_subscriber_identity, 0}).
+-define('MSISDN',					{v2_msisdn, 0}).
+-define('PDN Address Allocation',			{v2_pdn_address_allocation, 0}).
+-define('RAT Type',					{v2_rat_type, 0}).
+-define('Sender F-TEID for Control Plane',		{v2_fully_qualified_tunnel_endpoint_identifier, 0}).
+-define('Access Point Name',				{v2_access_point_name, 0}).
+-define('Bearer Contexts to be created',		{v2_bearer_context, 0}).
+-define('Bearer Contexts to be modified',		{v2_bearer_context, 0}).
+-define('Protocol Configuration Options',		{v2_protocol_configuration_options, 0}).
+-define('ME Identity',					{v2_mobile_equipment_identity, 0}).
+-define('APN-AMBR',					{v2_aggregate_maximum_bit_rate, 0}).
+-define('Bearer Level QoS',				{v2_bearer_level_quality_of_service, 0}).
+-define('EPS Bearer ID',                                {v2_eps_bearer_id, 0}).
+-define('Indication',                                   {v2_indication, 0}).
+
+-define('S1-U eNode-B', 0).
+-define('S1-U SGW',     1).
+-define('S5/S8-U SGW',  4).
+-define('S5/S8-U PGW',  5).
+-define('S5/S8-C SGW',  6).
+-define('S5/S8-C PGW',  7).
+-define('S11-C MME',    10).
+-define('S11/S4-C SGW', 11).
