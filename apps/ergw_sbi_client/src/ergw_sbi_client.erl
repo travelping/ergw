9c91173bf6cfd3d9fd5d6f0b0c189571ecaa5e2f
Andreas Schultz <andreas.schultz@travelping.com>
2021-07-06 17:47:48 +0200

    1st version of the SBI client

--- apps/ergw_sbi_client/src/ergw_sbi_client.erl
+++ apps/ergw_sbi_client/src/ergw_sbi_client.erl
@@ -17,6 +17,10 @@
     upf_selection/1
 ]).
 
+-ifdef(TEST).
+-export([from_session/1]).
+-endif.
+
 -ignore_xref([?MODULE]).
 
 %%% ============================================================================
@@ -27,8 +31,9 @@ start_link() ->
     Opts = [{hibernate_after, 5000}, {spawn_opt, [{fullsweep_after, 0}]}],
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], Opts).
 
-upf_selection(Data) ->
-    gen_server:call(?MODULE, {upf_selection, Data}).
+upf_selection(Session) ->
+    Query = from_session(Session),
+    gen_server:call(?MODULE, {upf_selection, Query}).
 
 setup(Config) ->
     gen_server:call(?MODULE, {setup, Config}).
@@ -62,6 +67,83 @@ terminate(_Reason, _State) ->
 %%% ============================================================================
 %%% Internal Functions
 %%% ============================================================================
+
+put_kv([K], V, M) ->
+    M#{K => V};
+put_kv([H|T], V, M) ->
+    maps:put(H, put_kv(T, V, maps:get(H, M, #{})), M).
+
+from_ip({_, _, _, _} = IP) ->
+    #{<<"ipv4Addr">> => iolist_to_binary(inet:ntoa(IP))};
+from_ip({_, _, _, _,_, _, _, _} = IP) ->
+    #{<<"ipv6Addr">> => iolist_to_binary(inet:ntoa(IP))}.
+
+split_location_info(<<MCC:3/bytes, MNC:3/bytes, Rest/binary>> = Bin, Size)
+  when byte_size(Bin) == Size ->
+    {{MCC, MNC}, Rest};
+split_location_info(<<MCC:3/bytes, MNC:2/bytes, Rest/binary>>, _Size) ->
+    {{MCC, MNC}, Rest}.
+
+from_session(Session) ->
+    maps:fold(fun from_session/3, #{}, Session).
+
+from_session('APN', V, Req) ->
+    Req#{<<"accessPointName">> => V};
+from_session('3GPP-IMSI', V, Req) ->
+    Req#{<<"supi">> => <<"imsi-", V/binary>>};
+from_session('3GPP-SGSN-MCC-MNC', <<MCC:3/bytes, MNC/binary>>, Req) ->
+    put_kv([<<"vPlmn">>, <<"plmnId">>], #{<<"mcc">> => MCC, <<"mnc">> => MNC}, Req);
+from_session('3GPP-SGSN-Address', V, Req) ->
+    put_kv([<<"vPlmn">>, <<"cpAddress">>], from_ip(V), Req);
+from_session('3GPP-SGSN-UP-Address', V, Req) ->
+    put_kv([<<"vPlmn">>, <<"upAddress">>], from_ip(V), Req);
+from_session('GTP-Version', v1, Req) ->
+    Req#{<<"protocolType">> => <<"GTPv1">>};
+from_session('GTP-Version', v2, Req) ->
+    Req#{<<"protocolType">> => <<"GTPv2">>};
+from_session('3GPP-MSISDN', V, Req) ->
+    Req#{<<"gpsi">> => <<"msisdn-", V/binary>>};
+from_session('3GPP-IMEISV' , V, Req) ->
+    Req#{<<"pei">> => <<"imeisv-", V/binary>>};
+
+from_session('CGI', V, Req) when is_binary(V) ->
+    case split_location_info(V, 10) of
+	{{MCC, MNC}, <<LAC:16, CI:16>>} ->
+	    Id = #{<<"plmnId">> => #{<<"mcc">> => MCC, <<"mnc">> => MNC},
+		   <<"lac">> => LAC,
+		   <<"cellId">> => CI},
+	    put_kv([<<"userLocationInfo">>, <<"cgi">>], Id, Req);
+	_ ->
+	    %% unable do decode CGI
+	    Req
+    end;
+from_session('SAI', V, Req) when is_binary(V) ->
+    case split_location_info(V, 10) of
+	{{MCC, MNC}, <<LAC:16, SAC:16>>} ->
+	    Id = #{<<"plmnId">> => #{<<"mcc">> => MCC, <<"mnc">> => MNC},
+		   <<"lac">> => LAC,
+		   <<"sac">> => SAC},
+	    put_kv([<<"userLocationInfo">>, <<"sai">>], Id, Req);
+	_ ->
+	    %% unable do decode SAI
+	    Req
+    end;
+from_session('RAI', V, Req) when is_binary(V) ->
+    case split_location_info(V, 9) of
+	{{MCC, MNC}, <<LAC:16, RAC:8>>} ->
+	    Id = #{<<"plmnId">> => #{<<"mcc">> => MCC, <<"mnc">> => MNC},
+		   <<"lac">> => LAC,
+		   <<"rac">> => RAC},
+	    put_kv([<<"userLocationInfo">>, <<"rai">>], Id, Req);
+	_ ->
+	    %% unable do decode RAI
+	    Req
+    end;
+
+from_session('QoS-Information' = K, V, Req) ->
+    Req#{K => V};
+from_session(_, _, Req) ->
+    Req.
 
 upf_selection(Data, #{upf_selection :=
   #{timeout := Timeout,uri := #{path := Path}} = Config} = State) ->
@@ -99,7 +181,7 @@ gun_open(#{pid := Pid} = Opts) when is_pid(Pid) ->
     end;
 gun_open(#{uri := #{host := Host, port := Port}, timeout := Timeout}) ->
     GunOpts = #{http2_opts => #{keepalive => infinity},
-		protocols => [http2, http]},
+		protocols => [http2]},
     case gun:open(Host, Port, GunOpts) of
 	{ok, Pid} ->
 	    case gun:await_up(Pid, Timeout) of
@@ -116,26 +198,41 @@ gun_open(#{uri := #{host := Host, port := Port}, timeout := Timeout}) ->
 gun_open(_) ->
     {error, badarg}.
 
-get_reponse(#{pid := Pid, stream_ref := StreamRef,
-  timeout := Timeout, acc := Acc} = Opts) ->
+get_reponse(#{pid := Pid, stream_ref := StreamRef, timeout := Timeout, acc := Acc} = Opts) ->
     %% @TODO: fix correct 'Timeout' calculation issue and add time of request finished
     case gun:await(Pid, StreamRef, Timeout) of
 	{response, fin, Status, Headers} ->
-	    {ok, #{status => Status, headers => Headers, response => Acc}};
+	    handle_response(#{status => Status, headers => Headers}, Acc);
 	{response, nofin, Status, Headers} ->
 	    get_reponse(Opts#{status => Status, headers => Headers});
 	{data, nofin, Data} ->
 	    get_reponse(Opts#{acc => <<Acc/binary, Data/binary>>});
 	{data, fin, Data} ->
-	    Status = maps:get(status, Opts),
-	    Headers = maps:get(headers, Opts),
-	    {ok, #{status => Status, headers => Headers,
-		   response => <<Acc/binary, Data/binary>>}};
+	    handle_response(Opts, <<Acc/binary, Data/binary>>);
 	{error, timeout} = Response ->
 	    Response;
 	{error, _Reason} = Response->
 	    Response
     end.
+
+handle_response(#{status := 200, headers := Headers}, Data) ->
+    case lists:keyfind(<<"content-type">>, 1, Headers) of
+	{_, <<"application/json">>} ->
+	    case jsx:decode(Data, [{labels, binary}, return_maps]) of
+		#{<<"ipv4Addr">> := IP} when is_binary(IP) ->
+		    inet:parse_ipv4_address(binary_to_list(IP));
+		#{<<"ipv6Addr">> := IP} when is_binary(IP) ->
+		    inet:parse_ipv6_address(binary_to_list(IP));
+		#{<<"fqdn">> := FQDN} when is_binary(FQDN) ->
+		    {ok, FQDN};
+		_ ->
+		    {error, invalid_payload}
+	    end;
+	_ ->
+		    {error, invalid_content_type}
+    end;
+handle_response(_, _) ->
+    {error, invalid_response}.
 
 parse_uri(URI) when is_list(URI) ->
     case uri_string:parse(URI) of
--- apps/ergw_sbi_client/test/ergw_sbi_client_SUITE.erl
+++ apps/ergw_sbi_client/test/ergw_sbi_client_SUITE.erl
@@ -1,135 +1,154 @@
 -module(ergw_sbi_client_SUITE).
 
--export([
-    all/0,
-    groups/0,
-    init_per_suite/1,
-    end_per_suite/1
-]).
+-compile([export_all, nowarn_export_all]).
 
--export([
-    upf_selection_ok/0,
-    upf_selection_ok/1,
-    upf_selection_error/0,
-    upf_selection_error/1
-]).
+-include_lib("gtplib/include/gtp_packet.hrl").
 
--define(TEST_CONFIG, #{
-    upf_selection_api => #{
-	timeout => 1000,
-	default => "fallback",
-	endpoint => "https://ptsv2.com/t/qx48x-1624877407/post"
-    }
-}).
-
--define(SELECT_UPF, #{
-    accessPointName => "string",
-    supi => "string",
-    vPlmn => #{
-	plmnId => #{
-	    mcc => "string",
-	    mnc => "string"
-	},
-	cpAddress => #{
-	    ipv4Addr => "198.51.100.1",
-	    ipv6Addr => "2001:db8:85a3::8a2e:370:7334",
-	    ipv6Prefix => "2001:db8:abcd:12::0/64"
-	},
-	upAddress => #{
-	    ipv4Addr => "198.51.100.1",
-	    ipv6Addr => "2001:db8:85a3::8a2e:370:7334",
-	    ipv6Prefix => "2001:db8:abcd:12::0/64"
-	}
-    },
-    protocolType => "GTPv1",
-    gpsi => "string",
-    pei => "string",
-    userLocationInfo => #{
-	cgi => #{
-	    plmnId => #{
-		mcc => "string",
-		mnc => "string"
-	    },
-	    lac => "string",
-	    cellId => "string"
-	},
-	sai => #{
-	    plmnId => #{
-		mcc => "string",
-		mnc => "string"
-	    },
-	    lac => "string",
-	    sac => "string"
-	},
-	lai => #{
-	    plmnId => #{
-		mcc => "string",
-		mnc => "string"
-	    },
-	    lac => "string"
-	},
-	rai => #{
-	    plmnId => #{
-		mcc => "string",
-		mnc => "string"
-	    },
-	    lac => "string",
-	    rac => "string"
-	},
-	ageOfLocationInformation => 0,
-	ueLocationTimestamp => "2021-06-30T15:01:36.097Z",
-	geographicalInformation => "string",
-	geodeticInformation => "string"
-    },
-    qualityOfService => "string"
-}).
+-define(match(Guard, Expr),
+	((fun () ->
+		  case (Expr) of
+		      Guard -> ok;
+		      V -> ct:pal("MISMATCH(~s:~b, ~s)~nExpected: ~p~nActual:   ~p~n",
+				   [?FILE, ?LINE, ??Expr, ??Guard, V]),
+			    error(badmatch)
+		  end
+	  end)())).
 
 all() ->
-    [
-	{group, successed},
-	{group, failed}
-    ].
-
-groups() ->
-    [
-	{successed, [sequence], [
-	    upf_selection_ok
-	]},
-	{failed, [sequence], [
-	    upf_selection_error
-	]}
+    [from_session,
+     upf_selection
     ].
 
 init_per_suite(Config) ->
-    {ok, _} = application:ensure_all_started(ergw_sbi_client),
-    _ = ergw_sbi_client_config:validate_options(fun ergw_sbi_client_config:validate_option/2, ?TEST_CONFIG),
+    [{ok, _} = application:ensure_all_started(App) ||
+	App <- [ranch, gun, ergw_sbi_client]],
+
+    ProtoOpts =
+	#{env =>
+	      #{dispatch =>
+		    cowboy_router:compile(
+		      [{'_', [{"/ok", sbi_h, []}]}])
+	       }},
+    {ok, _} = cowboy:start_clear(?MODULE, [], ProtoOpts),
+    Port = ranch:get_port(?MODULE),
+
+    TestCfg =
+	#{upf_selection =>
+	      #{timeout => 1000,
+		default => "fallback",
+		endpoint =>
+		    uri_string:recompose(
+		      #{host => "localhost", path => "/ok", port => Port, scheme => "http"})
+	       }
+	 },
+    _ = ergw_sbi_client_config:validate_options(fun ergw_sbi_client_config:validate_option/2, TestCfg),
+    ok = ergw_sbi_client:setup(TestCfg),
     Config.
 
-end_per_suite(Config) ->
-    Config.
+end_per_suite(_) ->
+    ok = cowboy:stop_listener(?MODULE),
+    ok.
 
-%% http://ptsv2.com/t/qx48x-1624877407
+%%--------------------------------------------------------------------
+session() ->
+    %% copied from ergw_core/pgw_SUITE
+    #{'Username' => <<"111111111111111/3520990017614823/440000000000/ATOM/TEXT/12345@example.net">>,
+      'Framed-Protocol' => 'GPRS-PDP-Context',
+      'Multi-Session-Id' => 15958412130338583375391261628607396908,
+      'TAI' => <<3,2,22,214,217>>,
+      '3GPP-SGSN-Address' => {127,127,127,127},
+      'ECGI' => <<3,2,22,8,71,9,92>>,
+      '3GPP-RAT-Type' => 1,
+      'Framed-IPv6-Pool' => <<"pool-A">>,
+      '3GPP-Charging-Id' => 1981737279,
+      '3GPP-Selection-Mode' => 0,
+      '3GPP-IMSI' => <<"111111111111111">>,
+      '3GPP-MSISDN' => <<"440000000000">>,
+      'Password' => <<"ergw">>,
+      '3GPP-GGSN-Address' => {127,0,0,1},
+      'Session-Start' => -576460456578330165,
+      'Calling-Station-Id' => <<"440000000000">>,
+      'Service-Type' => 'Framed-User',
+      '3GPP-GGSN-MCC-MNC' => <<"00101">>,
+      '3GPP-IMEISV' => <<"3520990017614823">>,
+      'Charging-Rule-Base-Name' => <<"m2m0001">>,
+      '3GPP-MS-TimeZone' => {10,0},
+      'Session-Id' => 15958412130338583375391261628607396909,
+      'Framed-Pool' => <<"pool-A">>,
+      'Diameter-Session-Id' => <<"zeus;201423479;105063168;207890578476">>,
+      'User-Location-Info' =>
+	  #{'ext-macro-eNB' =>
+		#ext_macro_enb{plmn_id = {<<"001">>, <<"001">>},
+			       id = rand:uniform(16#1fffff)},
+	    'TAI' =>
+		#tai{plmn_id = {<<"001">>, <<"001">>},
+		     tac = rand:uniform(16#ffff)}},
+      'Called-Station-Id' => <<"example.net">>,
+      'Node-Id' => <<"PGW-001">>,
+      'QoS-Information' =>
+	  #{'APN-Aggregate-Max-Bitrate-DL' => 1704125000,
+	    'APN-Aggregate-Max-Bitrate-UL' => 48128000,
+	    'Allocation-Retention-Priority' =>
+		#{'Pre-emption-Capability' => 1,
+		  'Pre-emption-Vulnerability' => 0,
+		  'Priority-Level' => 10},
+	    'Guaranteed-Bitrate-DL' => 0,
+	    'Guaranteed-Bitrate-UL' => 0,
+	    'Max-Requested-Bandwidth-DL' => 0,
+	    'Max-Requested-Bandwidth-UL' => 0,
+	    'QoS-Class-Identifier' => 8},
+      'NAS-Identifier' => <<"NAS-Identifier">>,
+      '3GPP-NSAPI' => 5,
+      '3GPP-PDP-Type' => 'IPv4',
+      '3GPP-SGSN-MCC-MNC' => <<"001001">>,
+      '3GPP-IMSI-MCC-MNC' => <<"11111">>,
 
-upf_selection_ok() ->
+      %% TBD:
+      'APN' => <<"example.net">>,
+      '3GPP-SGSN-UP-Address' => {127,127,127,127},
+      'GTP-Version' => v2
+     }.
+
+%%--------------------------------------------------------------------
+from_session() ->
+    [{doc, "Check mapping from session to request"}].
+from_session(_Config) ->
+    Req = ergw_sbi_client:from_session(session()),
+    ct:pal("Req: ~p~n", [Req]),
+
+    ?match(
+       #{'QoS-Information' :=
+	     #{'APN-Aggregate-Max-Bitrate-DL' := 1704125000,
+	       'APN-Aggregate-Max-Bitrate-UL' := 48128000,
+	       'Allocation-Retention-Priority' :=
+		   #{'Pre-emption-Capability' := 1,
+		     'Pre-emption-Vulnerability' := 0,'Priority-Level' := 10},
+	       'Guaranteed-Bitrate-DL' := 0,'Guaranteed-Bitrate-UL' := 0,
+	       'Max-Requested-Bandwidth-DL' := 0,
+	       'Max-Requested-Bandwidth-UL' := 0,'QoS-Class-Identifier' := 8},
+	 <<"accessPointName">> := <<"example.net">>,
+	 <<"gpsi">> := <<"msisdn-440000000000">>,
+	 <<"pei">> := <<"imeisv-3520990017614823">>,
+	 <<"protocolType">> := <<"GTPv2">>,
+	 <<"supi">> := <<"imsi-111111111111111">>,
+	 <<"vPlmn">> :=
+	     #{<<"cpAddress">> := #{<<"ipv4Addr">> := <<"127.127.127.127">>},
+	       <<"plmnId">> := #{<<"mcc">> := <<"001">>, <<"mnc">> := <<"001">>},
+	       <<"upAddress">> := #{<<"ipv4Addr">> := <<"127.127.127.127">>}}}, Req),
+    ok.
+
+%%--------------------------------------------------------------------
+upf_selection() ->
     [{doc, "Check upf_selection_api"}].
-upf_selection_ok(_Config) ->
-    case ergw_sbi_client:upf_selection(?SELECT_UPF) of
-	{ok, Response} ->
-	    ct:comment("erGW SBI UPF selection API request success result"),
-	    ct:pal("Response: ~p", [Response]);
-	Any ->
-	    ct:fail(Any)
-    end.
+upf_selection(_Config) ->
+    Session = session(),
 
-upf_selection_error() ->
-    [{doc, "Check request error behavior"}].
-upf_selection_error(_Config) ->
-    #{upf_selection_api := UPF} = ?TEST_CONFIG,
-    _ = ergw_sbi_client:setup(?TEST_CONFIG#{upf_selection_api => UPF#{endpoint => "https://none-existing.com"}}),
-    case ergw_sbi_client:upf_selection(#{}) of
-	{error, timeout} = Error ->
-	    ct:comment("erGW SBI request error result"),
-	    ct:pal("Response: ~p", [Error]);
-	Any ->
-	    ct:fail(Any)
-    end.
+    ?match({ok, {_,_,_,_}},
+	   ergw_sbi_client:upf_selection(Session#{'3GPP-MSISDN' => <<"440000000000">>})),
+    ?match({ok, {_,_,_,_,_,_,_,_}},
+	   ergw_sbi_client:upf_selection(Session#{'3GPP-MSISDN' => <<"440000000001">>})),
+    ?match({ok, F} when is_binary(F),
+	   ergw_sbi_client:upf_selection(Session#{'3GPP-MSISDN' => <<"440000000002">>})),
+    ?match({error, _},
+	   ergw_sbi_client:upf_selection(Session#{'3GPP-MSISDN' => <<"440000000009">>})),
+    ok.
